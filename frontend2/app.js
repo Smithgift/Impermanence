05201858a8f96b458ad7d192dd45915d0f2b0c5c
Matthew Schmidt <smithgift264@gmail.com>
2015-12-01 18:47:38 +0000

    Web3 now works.

---
+++ frontend2/app.js
@@ -0,0 +1,5 @@
+var Web3 = require('web3');
+window.web3 = new Web3();
+//window.web3.setProvider(new window.web3.providers.HttpProvider('the-impermanence-of-space.on.ether.camp:8555/sandbox/3c3eed7ababee9fe3e5fa49685471dbe335f7d9e/'))
+//console.log(window.web3.eth.accounts);
+//JSON.stringify(url.parse(window.location.href));
