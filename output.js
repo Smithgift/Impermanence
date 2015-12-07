16bee0c68722c8871f7ce3a84e4be0aba0a63bc2
Matthew Schmidt <smithgift264@gmail.com>
2015-12-07 11:14:37 -0800

    Deleted autogenerated files and more stuff.

--- output.js
+++
@@ -1,3 +0,0 @@
-var abi = 
-{"contracts":{"Galaxy":{"abi":"[{\"constant\":true,\"inputs\":[{\"name\":\"\",\"type\":\"uint256\"}],\"name\":\"galacticMap\",\"outputs\":[{\"name\":\"name\",\"type\":\"string\"}],\"type\":\"function\"},{\"constant\":true,\"inputs\":[{\"name\":\"s\",\"type\":\"uint256\"},{\"name\":\"x\",\"type\":\"uint8\"},{\"name\":\"y\",\"type\":\"uint8\"}],\"name\":\"getSector\",\"outputs\":[{\"name\":\"\",\"type\":\"uint8\"}],\"type\":\"function\"},{\"constant\":false,\"inputs\":[{\"name\":\"_name\",\"type\":\"string\"}],\"name\":\"addSystem\",\"outputs\":[],\"type\":\"function\"},{\"inputs\":[],\"type\":\"constructor\"}]\n"}},"version":"0.1.7-0/Release-Linux/g++/int linked to libethereum-1.1.0-0/Release-Linux/g++/int"}
-
