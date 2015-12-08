9c5e04f68e8c6609649e0c6f0546e13b197e2d7d
Matthew Schmidt <smithgift264@gmail.com>
2015-12-08 11:41:38 -0800

    Now should connect to an RPC node by default.

--- frontend2/app.js
+++ frontend2/app.js
@@ -1,5 +1,7 @@
 var Web3 = require('web3');
 window.web3 = new Web3();
+// Change this if you have a different RPC address.
+window.web3.setProvider(new window.web3.providers.HttpProvider('http://localhost:8545'))
 window.build = require('./abi');
 // Elegance later.
 //window.impermanence = require('./impermanence');
