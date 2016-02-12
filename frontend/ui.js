384cbf1ed0a6a7357237000fbf6eef2a752b3da7
Matthew Schmidt <smithgift264@gmail.com>
2016-02-12 13:56:19 -0800

    Split off system select into its own component.

--- frontend/ui.js
+++ frontend/ui.js
@@ -2,13 +2,21 @@ function PageViewModel() {
   this.nextSys = m.prop("");
 } 
 
+var SystemSelect = {
+  view: function(ctrl, args) {
+       return m("div", [
+        "System name:",
+        m("input", {oninput: m.withAttr("value", args.pvm.nextSys)})
+      ]);
+  }
+}
+
 var FrontPage = {
   view: function(ctrl, args) {
     return m("div", [
-      m("div", [
-        "System name:",
-        m("input", {oninput: m.withAttr("value", args.pvm.nextSys)})
-      ])
+      m.component(SystemSelect, {pvm: args.pvm}),
+      m("br"),
+      "Enter a system name to begin."
     ]);
   }
 }
