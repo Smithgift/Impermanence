ab49531954e91c7bf7a13ef39c3c18f6904151cc
Matthew Schmidt <smithgift264@gmail.com>
2016-02-12 13:10:05 -0800

    More cleanup. frontend2 -> frontend

--- jsonhack.sh
+++ jsonhack.sh
@@ -6,7 +6,7 @@ cd contracts/
 pwd
 solc --combined-json abi,bin *.sol > ../build.json
 cd -
-echo "module.exports = " > frontend2/abi.js
-cat build.json >> frontend2/abi.js
+echo "module.exports = " > frontend/abi.js
+cat build.json >> frontend/abi.js
 #echo "var Galaxy = web3.eth.contract(JSON.parse(build" >> frontend2/abi.js
 #echo ".contracts.Galaxy.abi))" >> frontend2/abi.js
--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 ./jsonhack.sh
 onchange 'dapp/**/*.sol' -- ./jsonhack.sh &
-cd frontend2
+cd frontend
 beefy app.js --live
