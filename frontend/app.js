d55dae7e5a1ef94e177f6085dc1ee347f47e7fc1
Matthew Schmidt <smithgift264@gmail.com>
2016-02-14 09:43:44 -0800

    Switched to a require system rather than a zillion script tags.

--- frontend/app.js
+++ frontend/app.js
@@ -3,9 +3,7 @@ window.web3 = new Web3();
 // Change this if you have a different RPC address.
 window.web3.setProvider(new window.web3.providers.HttpProvider('http://localhost:8545'))
 window.build = require('./abi');
-// Elegance later.
-//window.impermanence = require('./impermanence');
 
-//window.web3.setProvider(new window.web3.providers.HttpProvider('the-impermanence-of-space.on.ether.camp:8555/sandbox/3c3eed7ababee9fe3e5fa49685471dbe335f7d9e/'))
-//console.log(window.web3.eth.accounts);
-//JSON.stringify(url.parse(window.location.href));
+var $ = require("jquery");
+var ui = require("./ui");
+$(document).ready(function() {ui.init();});
--- frontend/ui.js
+++ frontend/ui.js
@@ -1,19 +1,23 @@
+var m = require("mithril");
+
 function PageViewModel() {
   this.nextSys = m.prop("");
-} 
+}
+
+var pvm = new PageViewModel();
 
 var SystemSelect = {
   controller: function(args) {
     return {
       changeSystem: function() {
-        m.route("/system/" + args.pvm.nextSys());
+        m.route("/system/" + pvm.nextSys());
       }
     };
   },
   view: function(ctrl, args) {
     return m("div", [
       "System name:",
-      m("input", {oninput: m.withAttr("value", args.pvm.nextSys)}),
+      m("input", {oninput: m.withAttr("value", pvm.nextSys)}),
       m("button", {onclick: ctrl.changeSystem}, ["Go!"])
     ]);
   }
@@ -28,14 +32,23 @@ var SystemMap = {
   view: function(ctrl, args) {
     return m("h1", [ctrl.name]);
   }
-}
+};
 
 var FrontPage = {
   view: function(ctrl, args) {
     return m("div", [
-      m.component(SystemSelect, {pvm: args.pvm}),
+      m.component(SystemSelect),
       m("br"),
       "Enter a system name to begin."
     ]);
   }
+};
+
+function init() {
+  m.route(document.body, "/", {
+    "/": m.component(FrontPage, {pvm: pvm}),
+    "/system/:name": m.component(SystemMap, {pvm: pvm})
+  });
 }
+
+exports.init = init;
