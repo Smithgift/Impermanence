2830d0daa340d134d431be7a9912c9af1ebaee4a
Matthew Schmidt <smithgift264@gmail.com>
2016-02-21 13:35:49 -0800

    Added tests for system

---
+++ src/system.js
@@ -0,0 +1,14 @@
+// ORM of a system.
+module.exports = function(web3, galaxy) {
+  function System(name) {
+    this.name = name;
+    this.hash = "0x" + web3.sha3(this.name);
+    this.refreshMap();
+  }
+
+  System.prototype.refreshMap = function() {
+    this.map = galaxy.getSystemMap(this.hash);
+  };
+
+  return System;
+}
---
+++ test/system.js
@@ -0,0 +1,38 @@
+assert = require('assert');
+var universe = require('../src/universe');
+var system = require('../src/system');
+
+describe('system', function() {
+  this.timeout(0);
+  
+  var System;
+
+  before('setup universe', function(done) {
+    var u = universe(web3);
+    u.createUniverse(function(galaxy) {
+      System = system(web3, galaxy);
+      done();
+    });
+  })
+
+  describe('#empty systems', function() {
+    var tauceti;
+
+    before('create empty system', function() {
+      tauceti = new System("tauceti");
+    });
+
+    it('has a name', function() {
+      assert.equal(tauceti.name, "tauceti");
+    });
+
+    it('has the right hash', function() {
+      assert.equal(tauceti.hash, "0x69807b079150d6528543919a437c8090cc59608e48ff072186c2edbc469c6cb1");
+    }); 
+
+    it('has nothing in the map', function() {
+      assert.deepEqual(tauceti.map.map((bn) => bn.toNumber()), Array.from({length: 225}, () => 0));
+    });
+  });
+});
+
