6f3268f7058150a26211b85b62c4ca04f3b1e918
Matthew Schmidt <smithgift264@gmail.com>
2016-03-18 12:14:05 -0700

    A little clean-up on the testrunner.
    + Now gets mocha.js and .css from offline. IMHO, a dapp getting something from
      a CDN is dangerous. Not that the end user will be running unit tests, but
      still...

--- test/runner.js
+++ test/runner.js
@@ -1,11 +1,3 @@
-var assert = require("chai").assert;
-
-describe("A test", function() {
-  it("better work", function() {
-    assert.equal(1, 1);
-  });
-});
-
 require("./meta");
 require("./universe");
 require("./system");
